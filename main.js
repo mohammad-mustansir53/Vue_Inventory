c90613a74c275cea05e941676c548fcd750ed7fe
mmustansir <mmustansir@trafix.com>
2024-02-12 15:34:50 +0500

    Computed Properties

--- main.js
+++ main.js
@@ -3,22 +3,26 @@
 var app = new Vue({
   el: '#app',
   data: {
+    brand: 'Vue Mastery',
     product: 'Socks',
-    image: './assets/vmSocks-green.jpg',
+    selectedVariant: 0,
+    //image: './assets/vmSocks-green.jpg',
     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-    inStock: false,
+    //inStock: false,
     onSale: true,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     variants: [
       {
         variantId: 2234,
         variantColor: 'green',
-        variantImage: './assets/vmSocks-green.jpg'
+        variantImage: './assets/vmSocks-green.jpg',
+        varianQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: 'blue',
-        variantImage: './assets/vmSocks-blue.jpg'
+        variantImage: './assets/vmSocks-blue.jpg',
+        varianQuantity: 0
       }
     ],
     sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
@@ -28,11 +32,32 @@ var app = new Vue({
     addToCart: function() {
       this.cart += 1
     },
-    updateProduct(variantImage) {
-      this.image = variantImage
+    updateProduct(index) {
+      this.selectedVariant = index
     },
     removeFromCart() {
       this.cart -= 1
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].varianQuantity
+    },
+    sale() {
+      if(this.onSale)
+      {
+        return this.brand + ' ' + this.product + ' are on sale!'
+      }
+      else {
+        return this.brand + ' ' + this.product + ' are not on sale'
+      }
+      
     }
   }
 })
