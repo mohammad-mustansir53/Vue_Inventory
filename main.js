d8e2cac8a6f5fd4d65d79b7d19c493414d799e5b
mmustansir <mmustansir@trafix.com>
2024-02-12 13:06:49 +0500

    List Rendering

--- main.js
+++ main.js
@@ -7,7 +7,19 @@ var app = new Vue({
     image: './assets/vmSocks-green.jpg',
     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
     inStock: true,
-    onSale: true
+    onSale: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green'    
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue'
+      }
+    ],
+    sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
   } 
 })
   
