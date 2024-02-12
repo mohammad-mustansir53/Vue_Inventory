7a10dbbe108ed36fb67b6122c17a762ec7a08e4a
mmustansir <mmustansir@trafix.com>
2024-02-12 12:26:40 +0500

    Added assets folder containing an image & added style.css file

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 //Add a description to the data object with the value "A pair of warm, fuzzy socks". Then display the description using an expression in an p element, underneath the h1.
 
 var app = new Vue({
-    el: '#app',
-    data: {
-      product: 'Socks',
-      description: 'These are socks'
-    } 
-  })
+  el: '#app',
+  data: {
+    product: 'Socks',
+    image: './assets/vmSocks-green.jpg',
+    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+  } 
+})
   
   
